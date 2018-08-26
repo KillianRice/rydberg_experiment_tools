9b000aaf03b36c55f8688fff59bfdde6c205c7b0
rogding <rogding@users.noreply.github.com>
2018-08-26 15:56:43 -0500

    Update makeAnalysisFolders.py
    makeAnalysisFolders_v2.py

--- makeAnalysisFolders.py
+++ makeAnalysisFolders.py
@@ -13,7 +13,7 @@ while isotope not in ['84','86','87','88']:
     isotope = raw_input('Please select valid isotope: ')
 
 #base fold should be wherever you are storing your data
-baseFolder = '..'
+baseFolder = 'C:\\Users\\Rydberg\\Documents\\Data'
 
 #change to the base folder
 os.chdir(baseFolder)
@@ -30,12 +30,14 @@ except:
 analysisFolder = baseFolder+'\\Analysis\\'+isotope+'Sr'
 dataFolder = baseFolder+'\\Raw_Data\\'+isotope+'Sr'
 
+#create the directories that will contain the data and analysis
 os.mkdir(analysisFolder+'\\'+baseName+dateWithPeriods)
 os.mkdir(dataFolder+'\\'+dateWithPeriods)
 
 todayAnalysisFolder = analysisFolder+'\\'+baseName+dateWithPeriods
 todayDataFolder = dataFolder+'\\'+dateWithPeriods
 
+#create the master batch files
 f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'.txt','w')
 f.close()
 f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Bg.txt','w')
