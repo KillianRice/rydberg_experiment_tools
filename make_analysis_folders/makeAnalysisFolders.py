adc0f182cb35b2a7173de15de94b2b1d080ca072
rogding <rogding@users.noreply.github.com>
2019-01-14 11:24:55 -0600

    Add files via upload

--- make_analysis_folders/makeAnalysisFolders.py
+++ make_analysis_folders/makeAnalysisFolders.py
@@ -1,7 +1,9 @@
 import os
 import datetime
 import shutil
+import sys
 
+ver = sys.version_info[0]
 #get date and create timestamps
 dateWithPeriods = datetime.datetime.today().strftime('%Y.%m.%d')
 dateWithoutPeriods = datetime.datetime.today().strftime('%Y%m%d')
@@ -13,7 +15,7 @@ while isotope not in ['84','86','87','88']:
     isotope = input('Please select valid isotope: ')
 
 #base folder should be wherever you are storing your data
-baseFolder = 'E:\\Data\\'
+baseFolder = 'F:\\'
 path_to_notepad = '"C:\\Program Files (x86)\\Notepad++\\notepad++.exe"'
 repository_url = 'https://github.com/KillianRice/rydberg_analysis.git'
 
@@ -49,16 +51,40 @@ todayAnalysisFolder = analysisFolder+'\\'+dateWithPeriods+'_'+baseName
 todayDataFolder = dataFolder+'\\'+dateWithPeriods
 
 #clone most up to date version of analysis code
-os.system('git clone ' + repository_url + ' ' + todayAnalysisFolder)
+if os.listdir(analysisFolder+'\\'+dateWithPeriods+'_'+baseName) == []:
+	os.system('git clone ' + repository_url + ' ' + todayAnalysisFolder)
 
 #create the master batch files
 master_batch_atom = todayDataFolder + '\\Files_' + dateWithoutPeriods + '.txt.'
-f = open(master_batch_atom,'w+')
-f.close()
-f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Bg.txt','w+')
-f.close()
-f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Counts.txt','w+')
-f.close()
+try:
+	f = open(master_batch_atom,'r')
+	f.close()
+except:
+	f = open(master_batch_atom,'w+')
+	f.close()
+
+try:
+	f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Bg.txt','r')
+	f.close()
+except:
+	f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Bg.txt','w+')
+	f.close()
+
+try:
+	f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Counts.txt','r')
+	f.close()	
+except:
+	f = open(todayDataFolder+'\\Files_'+dateWithoutPeriods+'_Counts.txt','w+')
+	f.close()
+	
+try:
+	f = open(todayDataFolder+'\\custom_scan.csv','r')
+	f.close()
+except:
+	f = open(todayDataFolder+'\\custom_scan.csv','w+')
+	f.close()
+
+os.system("pause")
 
 #open the master batch file for convenience
 os.system('START /B ' + path_to_notepad + ' ' + master_batch_atom)
